0eab9cc55db74a528bf57f671e78c320965f1849
Thomas Mulloy <twmulloy@gmail.com>
2016-12-05 21:00:43 -0800

    WebComponent Loader

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+  connect.server({
+    root: ['.'],
+    livereload: true,
+    port: 3000
+  });
+});
+
+gulp.task('default', ['connect'], function() {
+  // place code for your default task here
+});
